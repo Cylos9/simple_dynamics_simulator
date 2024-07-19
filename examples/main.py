5da67bb930fce12d59e9a47d7805db27484eaffa
cylos9 <bobdbl99@gmail.com>
2024-07-19 16:09:54 +0900

    add tractor trajectory

--- examples/main.py
+++ examples/main.py
@@ -133,8 +133,13 @@ if __name__ == "__main__":
     # Animation
     static_path = {"reference path":  np.array([reference_path_data["x_ref"], reference_path_data["y_ref"]])}
     
+    tractor_state = np.zeros((3, states.shape[1]))
+    
+    for  index in range(states.shape[1]):
+        tractor_state[:, index] =  model._compute_tractor_pose(states[:, index])
+    
     dynamic_path = {"trailer trajectory": np.array([states[0], states[1]]),
-                    # "tractor trajectory": np.array([states[0], states[1]]),
+                    "tractor trajectory": np.array([tractor_state[0], tractor_state[1]]),
                     }
     
     animator.run(
